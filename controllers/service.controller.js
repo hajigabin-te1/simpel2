1d9cdaf97dc9fab5d278be89debe33689cd844f0
hajigabin-te1 <hajigabin@github.com>
2026-04-23 23:59:46 +0800

    Kirim dulu sebelum lanjut projeknya

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -1,2 +1,23 @@
 import { validationResult } from "express-validator";
-import Service from "../models/Service.model.js";
+import ServiceType from "../models/Service.model.js";
+
+
+export const getService = async (req,res) => {
+    try {
+        const { isActive } = req.query;
+        const filter = {};
+
+        //cek layanan aktif kemudian buat filter
+        if ( isActive !== undefined) filter.isActive = isActive === 'true';
+        const service = await ServiceType.find(filter).sort({
+            urutan : 1,
+            namaLayanan : 1
+        });
+        
+    } catch (error) {
+        res.status(403).json({
+            message : error
+        });
+    }
+}
+
